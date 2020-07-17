1bdd917b8e466c5fafc54985cd9fb2b399b748ca
tig1tig1 <60502568+tig1tig1@users.noreply.github.com>
2020-07-17 15:44:53 +0200

    testamonials(done)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { FeaturesComponent } from './features/features.component';
+import { PriceComponent } from './price/price.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AboutComponent},
+{path: 'Features', component: FeaturesComponent},
+{path: 'Price', component: PriceComponent},
+  
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
